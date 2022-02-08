f4f193dd32d239344203af456397988832eb0055
Sharofboyev <sharofboyev.sarvar@gmail.com>
2022-02-08 08:25:19 +0100

    Initialization

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const config = require("./config");
+app.use(express.json());
+
+app.listen(config.port, () => {
+    console.log(`Listening on port ${config.port}...`)
+})
